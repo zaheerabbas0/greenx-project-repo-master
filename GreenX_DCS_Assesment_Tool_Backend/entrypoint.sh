5458200c5c25ddf97bcc8dd02360436e671c285d
zaheer <zaheerabbas@gmail.com>
2026-01-15 18:02:35 +0500

    migration

---
+++ GreenX_DCS_Assesment_Tool_Backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo " Waiting for MySQL..."
+until nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 1
+done
+echo " MySQL ready"
+
+echo " Running database migrations..."
+alembic upgrade head
+
+
+echo " Starting application..."
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 2
