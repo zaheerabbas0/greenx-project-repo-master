25ca65417a203f1fb62761bb5fb394b3f6be7d38
zaheer <zaheerabbas@gmail.com>
2026-01-15 19:02:26 +0500

    test mig

--- GreenX_DCS_Assesment_Tool_Backend/entrypoint.sh
+++ GreenX_DCS_Assesment_Tool_Backend/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
 
-echo " Waiting for MySQL..."
+echo "Waiting for MySQL..."
 until nc -z "$DB_HOST" "$DB_PORT"; do
   sleep 1
 done
-echo " MySQL ready"
+echo "MySQL ready"
 
-echo " Running database migrations..."
-#alembic upgrade head
+if [ "$#" -gt 0 ]; then
+  echo "Running custom command: $@"
+  exec "$@"
+fi
 
-
-echo " Starting application..."
+echo "Starting application..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 2
