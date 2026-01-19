70ff179d1941bb8db873ce4bf9c9f7ec868a83e2
zaheer <zaheerabbas@gmail.com>
2026-01-19 21:27:24 +0500

    set url

--- GreenX_DCS_Assesment_Tool_Backend/entrypoint.sh
+++ GreenX_DCS_Assesment_Tool_Backend/entrypoint.sh
@@ -5,6 +5,19 @@ echo "Waiting for MySQL..."
 until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --skip-ssl -e "SELECT 1;" >/dev/null 2>&1; do
   sleep 1
 done
+echo "MySQL is ready"
 
-echo "Starting backend..."
+# Check alembic_version table ONLY
+ALEMBIC_EXISTS=$(mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --skip-ssl "$DB_NAME" \
+  -N -s -e "SHOW TABLES LIKE 'alembic_version';")
+
+if [ -z "$ALEMBIC_EXISTS" ]; then
+  echo "Alembic history missing → stamping head"
+  alembic stamp head
+else
+  echo "Alembic history exists → upgrading if needed"
+  alembic upgrade head
+fi
+
+echo "Starting backend server..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
